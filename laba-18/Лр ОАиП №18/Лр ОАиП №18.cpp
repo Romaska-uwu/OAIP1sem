c1efe9e159c7d4a9bc4b3699109639b8ef091942
Romaska-uwu <149205053+Romaska-uwu@users.noreply.github.com>
2025-09-26 10:43:45 +0300

    Add files via upload

---
+++ laba-18/Лр ОАиП №18/Лр ОАиП №18.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Уравнение x^2 + 4x - 2
+double equation1(double x) {
+    return x * x + 4 * x - 2;
+}
+
+// Уравнение sin(x) + 0.1
+double equation2(double x) {
+    return sin(x) + 0.1;
+}
+
+// Метод дихотомии для поиска корня уравнения
+double dichotomyMethod(double (*equation)(double), double a, double b, double e) {
+    double c;
+    while (abs(b - a) > e) {
+        c = (a + b) / 2;
+        if (equation(c) == 0.0) {
+            break;
+        }
+        else if (equation(c) * equation(a) < 0) {
+            b = c;
+        }
+        else {
+            a = c;
+        }
+    }
+    return c;
+}
+
+int main() {
+    setlocale(LC_CTYPE, "Rus");
+    double a, b, e;
+    cout << "Введите границы интервала a и b: ";
+    cin >> a >> b;
+    cout << "Введите точность e: ";
+    cin >> e;
+
+    // Находим корень для уравнения x^2 + 4x - 2
+    double root1 = dichotomyMethod(equation1, a, b, e);
+    cout << "Корень уравнения x^2 + 4x - 2: " << root1 << endl;
+
+    // Находим корень для уравнения sin(x) + 0.1
+    double root2 = dichotomyMethod(equation2, a, b, e);
+    cout << "Корень уравнения sin(x) + 0.1: " << root2 << endl;
+
+    return 0;
+}
---
+++ laba-18/Лр ОАиП №18_2/Лр ОАиП №18_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cstdarg>
+
+using namespace std;
+
+int fmin(int count, ...) {
+    va_list args;
+    va_start(args, count);
+
+    int min = va_arg(args, int);
+    for (int i = 1; i < count; i++) {
+        int temp = va_arg(args, int);
+        if (temp < min) {
+            min = temp;
+        }
+    }
+
+    va_end(args);
+    return min;
+}
+
+int main() {
+    setlocale(LC_CTYPE, "Rus");
+    // Вызов функции fmin с различным количеством параметров
+    int min1 = fmin(3, 10, 5, 8);
+    cout << "Минимальное число: " << min1 << endl;
+
+    int min2 = fmin(4, 15, 3, 7, 12);
+    cout << "Минимальное число: " << min2 << endl;
+
+    int min3 = fmin(5, 20, 25, 18, 30, 15);
+    cout << "Минимальное число: " << min3 << endl;
+
+    return 0;
+}
